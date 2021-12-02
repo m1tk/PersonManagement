f01f9a0ecdb047de9685e67ef85ec2c6e5fd4a58
Mohammed Salih <92683109+m1tk@users.noreply.github.com>
2021-12-02 22:45:40 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace guigui;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
+}
